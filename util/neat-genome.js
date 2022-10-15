b4125aa205186804248cf2f08574ccf440d23dc8
Björn Þór Jónsson <bangsi@bthj.is>
2022-10-15 23:25:37 +0200

    Adding neatjs import to genome util module.

--- util/neat-genome.js
+++ util/neat-genome.js
@@ -1,3 +1,5 @@
+import neatjs from 'neatjs';
+
 // check if the member offspring has the function createOffspringAsexual
 // - if not, create a new neatjs instance from the offspring data
 export function getParentInstanceFromMember( member ) {
